f772dacd37fc60912d5cf0e8f4796a9086ec4f1f
Prem <prem.rajah@gmail.com>
2019-10-04 21:17:11 +0100

    select field validation

--- src/app/tdf/tdf.component.ts
+++ src/app/tdf/tdf.component.ts
@@ -7,16 +7,20 @@ import { User } from '../user';
   styleUrls: ['./tdf.component.scss']
 })
 export class TdfComponent implements OnInit {
-
   topics = ['Angular', 'React', 'Vue'];
+  topicHasError = true;
 
-  userModel = new User('', '', 0, '', '', true);
+  userModel = new User('', '', 0, 'default', '', false);
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  validateTopic(value) {
+    if (value === 'default') {
+      this.topicHasError = true;
+    } else {
+      this.topicHasError = false;
+    }
   }
-
-
-
 }
